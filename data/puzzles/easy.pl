53947a4f04c8d65e9a9b7a4baa6f4c72b638547d
calebia <nivea.calebia.felix.santos@ccc.ufcg.edu.br>
2025-04-06 22:06:52 -0300

    Games nivel Medio

--- data/puzzles/easy.pl
+++ data/puzzles/easy.pl
@@ -49,7 +49,7 @@ puzzles([
           [filled, filled, filled, filled],
           [marked, filled, filled, marked] ],
 
-          [[1, 1], [4], [4], [2] ], % rowsHints
+          [[1, 1], [4], [4], [2] ],       % rowsHints
           [[3], [3], [3], [3] ],          % colsHints
           easy
     )
